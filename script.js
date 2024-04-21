c36a7f101edd815650aec0c38503e44f4bdde909
Nikhil <nikhilverma0062@gmail.com>
2024-04-21 13:05:13 +0530

    chassing after same flips

--- script.js
+++ script.js
@@ -1,24 +1,62 @@
-let botScore=0;
-let userScore=0;
+var botScore=0;
+var userScore=0;
+var botC=0;
 
-let head=document.querySelector("#head");
-let tail=document.querySelector("#tail");
-let coin=document.querySelector("#fig");
-let user=document.querySelector("#user");
-let bot=document.querySelector("#bot");
-let msg=document.querySelector("#msg");
+const head=document.querySelector("#head");
+const tail=document.querySelector("#tail");
+const coin=document.querySelector("#fig");
+const user=document.querySelector("#user");
+const comp=document.querySelector("#bot");
+const msg=document.querySelector("#msg");
 
 const botChoice=()=>{
     let opt=["head","tail"];
-    let botC=Math.floor(Math.random());
+    let botC=Math.floor(Math.random()*2);
     return opt[botC];
 }
 
+
+
+const playGame=(userChoice)=>{
+    if(userChoice===botC){
+        win(botC);
+    }else{
+        loss(botC);
+    }
+}
+
+function win(botC){
+    userScore++;
+    user.innerText=userScore;
+    msg.style.backgroundColor="green";
+    msg.innerText="you won the toss";
+
+    if(botC=="head"){
+        coin.style.animation="flipHead 3s forwards";
+    }else{
+        coin.style.animation="flipTail 3s forwards";
+    }
+}
+
+function loss(botC){
+    botScore++;
+    comp.innerText=botScore;
+    msg.style.backgroundColor="red";
+    msg.innerText="you lost the toss";
+    if(botC=="head"){
+        coin.style.animation="flipHead 3s forwards";
+    }else{
+        coin.style.animation="flipTail 3s forwards";
+    }
+}
+
 head.addEventListener("click",()=>{
-    let bot=botChoice();
-    console.log("head",bot);
+    botC=botChoice();
+    const userChoice=head.getAttribute("id");
+    playGame(userChoice);
 });
 tail.addEventListener("click",()=>{
-    let bot=botChoice();
-    console.log("tail",bot);
+    botC=botChoice();
+    userChoice=tail.getAttribute("id");
+    playGame(userChoice);
 });
